2661211b5ee19299115e7369ad9c0255f2ecb9c5
therealvio <41095688+therealvio@users.noreply.github.com>
2023-01-05 16:27:17 +1100

    Adds basic subtitle implementation and tweaks footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,10 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-black border-t border-white">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
+        <div className="py-28 flex flex-col lg:flex-row items-center justify-center">
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="https://nextjs.org/docs/basic-features/pages"
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,8 +1,13 @@
+import { SUBTITLE } from "../lib/constants"
+
 const Intro = () => {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="font-punkboy text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+    <section className="float-left flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <h1 className="font-punkboy text-5xl md:text-8xl tracking-tighter leading-tight md:pr-8">
         IWASNT
+      </h1>
+      <h1 className="font-punkboy text-5xl text-cyan md:text-8xl tracking-tighter leading-tight md:pr-8">
+        [{SUBTITLE}]
       </h1>
     </section>
   )
